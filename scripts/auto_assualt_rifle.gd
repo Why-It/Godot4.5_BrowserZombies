8deef400c55b4430a095b5610691624a1872ea95
Why-It <60483679+Why-It@users.noreply.github.com>
2025-10-08 14:33:25 -0400

    Weapons Update
    Adding functionality to weapons
    Small fixes to enemy colliders

--- scripts/auto_assualt_rifle.gd
+++ scripts/auto_assualt_rifle.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+var cur_reserve_ammo = 0
+var max_reserve_ammo = 126
+var cur_mag_ammo = 0
+var max_mag_ammo = 21
 var damage = 25
 var automatic = true
 var fire_rate = .1
@@ -12,7 +16,6 @@ const fire_anim_name = "guns/ar_fire"
 @onready var anim_player = $AnimationPlayer
 @onready var bhole_decal = preload("res://blueprints_prefabs/bullet_hole_placeholder.tscn")
 @onready var bhole_wound_decal = preload("res://blueprints_prefabs/bullet_hole_wound.tscn")
-
 
 @onready var weapon_icon = "res://textures/autorifle128.png"
 
@@ -27,11 +30,45 @@ const fire_anim_name = "guns/ar_fire"
 @onready var crosshair_comp = $crosshair
 @onready var crosshair_img = $crosshair/CenterContainer/TextureRect
 
+var can_gun_fire = true
 var is_gun_done_firing = false
+var is_reloading = false
 
 func _ready():
-	pass
+	cur_mag_ammo = max_mag_ammo
+	cur_reserve_ammo = max_reserve_ammo
 
+func _mag_reload():
+	if cur_mag_ammo != max_mag_ammo:
+		if cur_reserve_ammo > 0:
+			anim_player.play("guns/tg_reload")
+			is_reloading = true
+		else:
+			pass
+	else:
+		pass
+
+func _mag_reloaded():
+	var transfering_ammo = 0
+	if cur_reserve_ammo >= max_mag_ammo:
+		cur_reserve_ammo += cur_mag_ammo
+		transfering_ammo = max_mag_ammo
+	else:
+		transfering_ammo = cur_reserve_ammo
+	cur_reserve_ammo -= transfering_ammo
+	cur_mag_ammo = transfering_ammo
+	is_reloading = false
+	can_gun_fire = true
+
+
+func _trigger_pulled():
+	anim_player.stop()
+	if cur_mag_ammo > 0:
+		anim_player.play(fire_anim_name)
+		can_gun_fire = true
+		cur_mag_ammo -= 1
+	else:
+		can_gun_fire = false
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "guns/ar_fire":
